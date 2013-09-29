1070dc1118a63fc6e6220003d4437302c9aa1f29
Aviad Reich <aviadreich@gmail.com>
2013-09-29 22:18:22 +0300

    fix README example (missing "user=>" prompt)

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clj-btc "0.1.0"
+(defproject clj-btc "0.2.0-SNAPSHOT"
   :description "Clojure bindings for the original (C++) Bitcoin Client"
   :url "https://github.com/aviad/clj-btc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.3"]
-                 [http-kit "2.1.11"]])
+                 [http-kit "2.1.11"]
+                 [org.clojure/core.typed "0.2.13"]])
