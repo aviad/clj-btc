ac67ce56ea3bca5cd854210411a431bccdf75876
Aspasia Beneti <aspasia.beneti@gmail.com>
2018-10-09 09:45:45 +0200

    Upgraded http-kit for compatibility with Java 9+

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-frc "0.1.1"
+(defproject clj-frc "0.1.2"
   :description "Clojure bindings for the original (C++) Bitcoin Client"
   :url "https://github.com/PIENews/clj-btc"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [http-kit "2.1.19"]
+                 [http-kit "2.3.0-beta2"]
                  [com.taoensso/timbre "4.10.0"]])
